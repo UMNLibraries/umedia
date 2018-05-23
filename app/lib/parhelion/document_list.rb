99fdd59bda3c5694b46b88c595331723924330d1
chadfennell <libsys@gmail.com>
2018-05-23 09:50:11 -0500

    a list of documents

---
+++ app/lib/parhelion/document_list.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Parhelion
+  # A light wrapper around an RSolr result hash
+  class DocumentList
+    include Enumerable
+    attr_reader :results,
+                :num_found,
+                :start,
+                :field_order,
+                :doc_klass
+
+    def initialize(results: [],
+                   num_found: 0,
+                   start: 0,
+                   field_order: [],
+                   doc_klass: Document)
+
+      @results     = results
+      @num_found   = num_found
+      @start       = start
+      @field_order = field_order
+      @doc_klass   = doc_klass
+    end
+
+    def each(&block)
+      results.each do |doc|
+        yield doc_klass.new(doc_hash: doc, field_order: field_order)
+      end
+    end
+  end
+end
