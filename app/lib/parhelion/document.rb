bed7d165803bf7e3d06267a5e1e533e5d26435bf
chadfennell <libsys@gmail.com>
2018-05-23 10:26:19 -0500

    adds dynamic accessors for document fields

--- app/lib/parhelion/document.rb
+++ app/lib/parhelion/document.rb
@@ -7,7 +7,23 @@ module Parhelion
     def initialize(doc_hash: {}, field_order: [], field_klass: Field)
       @doc_hash    = doc_hash
       @field_order = field_order
-      @field_klas  = field_klass
+      @field_klass = field_klass
+    end
+
+    def id
+      doc_hash.fetch('id')
+    end
+
+    def method_missing(method_name, *arguments, &block)
+      if method_name.to_s =~ /field_(.*)/
+        field($1, field_value($1))
+      else
+        super
+      end
+    end
+
+    def respond_to_missing?(method_name, include_private = false)
+      method_name.to_s.start_with?('field_') || super
     end
 
     def fields
@@ -27,12 +43,17 @@ module Parhelion
 
     def field_values
       field_order.map do |name|
-        field(name)
+        field(name, field_value(name))
       end
     end
 
-    def field(name)
-      Field.new name: name, value: doc_hash.fetch(name, '')
+    def field(name, value)
+      field = doc_hash.key?(name) ? field_klass : MissingField
+      field.new name: name, value: value
+    end
+
+    def field_value(field)
+      doc_hash.fetch(field, '')
     end
   end
 end
--- test/lib/parhelion/document_test.rb
+++ test/lib/parhelion/document_test.rb
@@ -13,6 +13,24 @@ module Parhelion
           ]
         )
       end
+
+      it 'responds with a field when given a message prefixed with "field"' do
+        doc_hash = { 'title' => 'foo', 'creator' => %w[blah blergh] }
+        doc = Document.new(doc_hash: doc_hash,
+                           field_order: %w[creator title corgi])
+        doc.field_title.must_equal(Field.new(value: 'foo', name: 'title'))
+      end
+
+      describe 'when given a field name that does not exist' do
+        it 'responds with a an empty field' do
+          doc_hash = { 'title' => 'foo', 'creator' => %w[blah blergh] }
+          doc = Document.new(doc_hash: doc_hash,
+                            field_order: %w[creator title corgi])
+          doc.field_not_gonna_happen.must_be_kind_of(MissingField)
+          doc.field_not_gonna_happen.missing?.must_equal true
+          doc.field_not_gonna_happen.exists?.must_equal false
+        end
+      end
     end
   end
 end
