74795b9c681022aa22ac86bca31b9367d7f52c4d
chadfennell <libsys@gmail.com>
2018-06-05 12:22:54 -0500

    adds a webpacker mount and a CSP config to allow the web app to connect to it; NOTE: HMR is not working

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -23,3 +23,17 @@
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
 # Rails.application.config.content_security_policy_report_only = true
+
+
+Rails.application.config.content_security_policy do |policy|
+  policy.default_src :self, :https
+  policy.font_src    :self, :https, :data
+  policy.img_src     :self, :https, :data, 'www.lib.umn.edu'
+  policy.object_src  :none
+  policy.script_src  :self, :https
+  policy.style_src   :self, :https, :unsafe_inline
+
+  # Specify URI for violation reports
+  # policy.report_uri "/csp-violation-report-endpoint"
+  policy.connect_src :self, :https, 'http://0.0.0.0:3035', 'ws://0.0.0.0:3035' if Rails.env.development?
+end
