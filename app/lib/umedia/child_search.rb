e16059e132f878bf5236d284084bcad2b183d4a6
chadfennell <libsys@gmail.com>
2018-06-22 10:47:20 -0500

    search compound children

---
+++ app/lib/umedia/child_search.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Umedia
+  # Search for Child records
+  class ChildSearch
+    attr_reader :q, :fl, :parent_id, :client, :document_list_klass
+    def initialize(q: '',
+                   fl: 'title, id, parent_id',
+                   parent_id: '',
+                   client: SolrClient,
+                   document_list_klass: Parhelion::DocumentList)
+      @q = q
+      @fl = fl
+      @parent_id = parent_id
+      @client = client
+      @document_list_klass = document_list_klass
+    end
+
+    def documents
+      document_list_klass.new(results: response['response']['docs'])
+    end
+
+    def highlighting
+      response['highlighting']
+    end
+
+    private
+
+    def response
+      @response ||= client.new.solr.get 'child_search', params: {
+        q: q,
+        'q.alt': '*:*',
+        sort: 'child_index asc',
+        hl: 'on',
+        fl: fl,
+        hl: 'on',
+        'hl.method': 'unified',
+        fq: ["parent_id:\"#{parent_id}\""],
+        rows: 2000
+      }
+    end
+  end
+end
---
+++ test/lib/umedia/child_search_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+module Umedia
+  class ChildSearchTest < ActiveSupport::TestCase
+    it 'searches for children of a document' do
+      parent_id = 9942
+      q = ''
+      document_list_klass = Minitest::Mock.new
+      document_list_obj = Minitest::Mock.new
+      document_list_klass.expect :new, [{foo: 'bar'}], [{:results=>[{:id=>"sdfsdf:sdf"}]}]
+      client = Minitest::Mock.new
+      response = Minitest::Mock.new
+      solr = Minitest::Mock.new
+      client.expect :new, response, []
+      response.expect :solr, solr, []
+      solr.expect :get, {'highlighting' => 'highlighting here', 'response' => { 'docs' =>[{id: 'sdfsdf:sdf'}]} }, ["child_search", {:params=>{:q=>"", :"q.alt"=>"*:*", :sort=>"child_index asc", :hl=>"on", :fl=>"title, id, parent_id", :"hl.method"=>"unified", :fq=>["parent_id:\"9942\""], :rows=>2000}}]
+      search = ChildSearch.new(q: '', parent_id: parent_id, client: client, document_list_klass: document_list_klass)
+      search.documents.must_equal [{:foo=>"bar"}]
+      search.highlighting.must_equal 'highlighting here'
+      document_list_klass.verify
+      client.verify
+      response.verify
+      solr.verify
+    end
+  end
+end
