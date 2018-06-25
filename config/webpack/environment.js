445d6e8467ec5196dfb93845cfa012d8d15e00f9
chadfennell <libsys@gmail.com>
2018-06-25 11:56:20 -0500

    expose jquery globally via ProvidePlugin to make libraries that expect a global jquery happy

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+// See: https://github.com/rails/webpacker/blob/master/docs/webpack.md#plugins
+// And: https://webpack.js.org/plugins/provide-plugin/
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+    bootstrap: 'bootstrap',
+  })
+)
 
 module.exports = environment
