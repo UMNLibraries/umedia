671b08d531bbaaa38826f2dce0e3cf3cb360e918
chadfennell <libsys@gmail.com>
2018-07-24 11:00:41 -0500

    adds a redis instance for caching

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,14 +18,12 @@ Rails.application.configure do
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
-
-    config.cache_store = :memory_store
+    config.cache_store = :redis_cache_store, {url: "redis://redis_cache:6379/0/cache"}
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
-
     config.cache_store = :null_store
   end
 
