4bd313998873029a45bb57b9168ac86a2f7ea818
chadfennell <libsys@gmail.com>
2018-07-25 15:26:10 -0500

    expose the thumbnail function globally

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,7 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 
+
 // See: https://github.com/rails/webpacker/blob/master/docs/webpack.md#plugins
 // And: https://webpack.js.org/plugins/provide-plugin/
 environment.plugins.prepend(
@@ -10,6 +11,7 @@ environment.plugins.prepend(
     jQuery: 'jquery',
     jquery: 'jquery',
     bootstrap: 'bootstrap',
+    noThumbnail: 'noThumbnail',
   })
 )
 
