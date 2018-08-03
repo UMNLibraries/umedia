20d6857b249883094b6f6d33ec5a9a9ffb49752e
chadfennell <libsys@gmail.com>
2018-08-03 10:05:11 -0500

    a class to handle IIIF info

---
+++ app/lib/parhelion/cdm_iiif.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Parhelion
+  # One place to store the cdm connection info
+  class CdmIiif
+    attr_reader :id, :collection, :endpoint, :rest_client_klass
+    def initialize(id: :MISSING_ID,
+                   collection: :MISSING_COLLECTION,
+                   endpoint: ENV['PARHELION_CDM_ENDPOINT'],
+                   rest_client_klass: RestClient )
+      @id = id
+      @collection = collection
+      @endpoint = endpoint
+      @rest_client_klass = rest_client_klass
+    end
+
+    def info
+      @info ||= JSON.parse(rest_client_klass.get(info_url).body)
+    end
+
+    private
+
+    def info_url
+      "#{endpoint}/digital/iiif/#{collection}/#{id}/info.json"
+    end
+  end
+end
