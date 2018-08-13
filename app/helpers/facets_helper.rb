1453689f91c981bde63d254366dfaec034bf99e6
chadfennell <libsys@gmail.com>
2018-08-13 09:42:31 -0600

    a helper on whether or not to include the modal JS code

--- app/helpers/facets_helper.rb
+++ app/helpers/facets_helper.rb
@@ -1,2 +1,9 @@
 module FacetsHelper
+  def modal_query(new_param, params)
+    URI.parse(facets_path(params.merge(new_param))).query
+  end
+
+  def data_controller(params)
+    (params[:modal] == 'on') ? 'data-controller=searches' : ''
+  end
 end
