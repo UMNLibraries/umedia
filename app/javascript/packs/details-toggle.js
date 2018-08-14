35e605828d8456a39ceddfacfe798ee2aa888bcf
chadfennell <libsys@gmail.com>
2018-08-14 11:26:14 -0600

    fixes: details not hiding

--- app/javascript/packs/details-toggle.js
+++ app/javascript/packs/details-toggle.js
@@ -4,13 +4,13 @@ $( document ).on('turbolinks:load', function() {
     $('.details-pill').addClass('active');
     $('.citation-pill').removeClass('active');
     $('.item-metadata .citation').addClass('hide');
-    $('.item-details .citation').removeClass('hide');
+    $('.item-details').removeClass('hide');
   });
   $('.citation-pill a').click(function(event) {
     event.preventDefault();
     $('.details-pill').removeClass('active');
     $('.citation-pill').addClass('active');
     $('.item-metadata .citation').removeClass('hide');
-    $('.item-details .citation').addClass('hide');
+    $('.item-details').addClass('hide');
   });
 });
