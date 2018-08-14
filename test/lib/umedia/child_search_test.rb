589ec5ccc5975e5367a0f1f65e86ae9b33102b5e
chadfennell <libsys@gmail.com>
2018-08-14 08:56:21 -0600

    we get viewer_type as well now

--- test/lib/umedia/child_search_test.rb
+++ test/lib/umedia/child_search_test.rb
@@ -13,7 +13,7 @@ module Umedia
       solr = Minitest::Mock.new
       client.expect :new, response, []
       response.expect :solr, solr, []
-      solr.expect :get, {'highlighting' => 'highlighting here', 'response' => { 'docs' =>[{id: 'sdfsdf:sdf'}]} }, ["child_search", {:params=>{:q=>"", :"q.alt"=>"*:*", :sort=>"child_index asc", :hl=>"on", :fl=>"title, id, parent_id, child_viewer_types, object", :"hl.method"=>"unified", :fq=>["parent_id:\"9942\""], :rows=>2000}}]
+      solr.expect :get, {'highlighting' => 'highlighting here', 'response' => { 'docs' =>[{id: 'sdfsdf:sdf'}]} }, ["child_search", {:params=>{:q=>"", :"q.alt"=>"*:*", :sort=>"child_index asc", :hl=>"on", :fl=>"title, id, object, parent_id, child_viewer_types, viewer_type", :"hl.method"=>"unified", :fq=>["parent_id:\"9942\""], :rows=>2000}}]
       search = ChildSearch.new(q: '', parent_id: parent_id, client: client, item_list_klass: item_list_klass)
       search.items.must_equal [{:foo=>"bar"}]
       search.highlighting.must_equal 'highlighting here'
