2acbd4c02bc99e63e82aac9cbac96b3dc9603113
chadfennell <libsys@gmail.com>
2018-08-14 08:17:35 -0600

    a more complete citation test

--- test/lib/umedia/citation/cite_test.rb
+++ test/lib/umedia/citation/cite_test.rb
@@ -16,10 +16,28 @@ module Umedia
           formatters: [FakeFormatter]
         }]
         doc = Minitest::Mock.new
-        doc_field = Minitest::Mock.new
-        doc.expect 'field_creator.value', 'Ted', []
+        field = Minitest::Mock.new
+        doc.expect :field_creator, field, []
+        field.expect :value, 'Ted', []
         cite = Cite.new(doc: doc, mappings: mappings)
         cite.to_s.must_equal 'WOWO(Ted).'
+        doc.verify
+        field.verify
+      end
+
+      describe 'when a value is set' do
+        it 'uses the value' do
+          mappings = [{
+            name: 'publisher',
+            value: "UMN",
+            prefix: '',
+            suffix: '',
+            formatters: []
+          }]
+          doc = {}
+          cite = Cite.new(doc: doc, mappings: mappings)
+          cite.to_s.must_equal 'UMN'
+        end
       end
     end
   end
