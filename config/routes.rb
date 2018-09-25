d06e3fabc220c4f85481c96c9459beb81464573e
chadfennell <libsys@gmail.com>
2018-09-25 15:35:03 -0500

    downloads

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   root to: 'searches#index'
   get 'search', to: 'searches#index', as: 'searches'
   get 'item/:id/(:child_id)', to: 'items#show', as: 'item'
+  get 'downloads/:id', to: 'downloads#show', as: 'downloads'
   get 'viewers/:id/(:child_id)', to: 'viewers#show', as: 'viewer'
   get 'child_search/:id/:active_child_id/:rows/(:q)', to: 'child_searches#index', as: 'child_search'
   get 'thumbnails/:item_id' => 'thumbnails#update', as: 'thumbnail'
