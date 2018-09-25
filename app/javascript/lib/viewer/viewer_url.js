249246b3077d0bbb21e2f76f8183199fe2096dbd
chadfennell <libsys@gmail.com>
2018-09-25 12:47:20 -0500

    remember which page has been selected

--- app/javascript/lib/viewer/viewer_url.js
+++ app/javascript/lib/viewer/viewer_url.js
@@ -7,31 +7,33 @@ export default stampit({
     history: window.history,
     origin: document.location.origin,
     search: document.location.search,
-    path_name: document.location.pathname,
+    pathName: document.location.pathname,
     id: false,
     child_id: false,
+    sidebar_page: null,
   },
   init({ history = this.history,
          origin = this.origin,
          search = this.search,
          q = this.q,
          id = this.id,
-         child_id = this.child_id}) {
+         child_id = this.child_id,
+         sidebar_page = 1 }) {
 
     this.history = history;
-    const default_path = this.path_name;
-
+    this.sidebar_page = sidebar_page;
+    const defaultPath = this.pathName;
 
     const idsPath = function() {
       return [id, child_id].filter(id => id).join('/')
     }
 
     const pathName = function() {
-      return (idsPath() != '') ? `/item/${idsPath()}` : default_path;
+      return (idsPath() != '') ? `/item/${idsPath()}` : defaultPath;
     }
 
     const queryParams = function() {
-      return { ...queryString.parse(search), ...{query: q} }
+      return { ...queryString.parse(search), ...{query: q}, ...{sidebar_page: sidebar_page} }
     }
 
     this.newURL = function() {
--- test/javascript/lib/viewer/viewer_url.test.js
+++ test/javascript/lib/viewer/viewer_url.test.js
@@ -1,20 +1,18 @@
-import ViewerUrl from '../../../../app/javascript/lib/viewer/viewer_url.js';
 
+import ViewerUrl from '../../../../app/javascript/lib/viewer/viewer_url.js';
 it('updates a search parameter', async () => {
   const pushState = jest.fn()
   const history = {
     pushState: pushState,
   }
-
-
   const viewer = ViewerUrl({
     origin: 'http://localhost/',
     search: {"foo": "bar"},
     q: 'minnesota',
     id: 'coll:1234',
-    child_id: 'coll:4321',
+    childId: 'coll:4321',
     history: history,
   });
   viewer.update();
-  expect(pushState.mock.calls[0]).toEqual( [ null,'', 'http://localhost//item/coll:1234/coll:4321?query=minnesota' ]);
+  expect(pushState.mock.calls[0]).toEqual( [ null,'', 'http://localhost//item/coll:1234?query=minnesota&sidebar_page=1' ]);
 });
