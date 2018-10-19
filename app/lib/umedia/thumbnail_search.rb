027c9e03a37496d305a1680f5a2dd9f9ff491c48
chadfennell <libsys@gmail.com>
2018-10-19 11:00:33 -0500

    make thumbnail cache warmer configurable by query - e.g. will allow for per-item cache warming via primary_id:foo

--- app/lib/umedia/thumbnail_search.rb
+++ app/lib/umedia/thumbnail_search.rb
@@ -3,16 +3,20 @@
 module Umedia
   # Bootstrap thumbnail cache
   class ThumbnailSearch
-    attr_reader :page, :client, :thumb_klass
-    def initialize(page: 1,
+    attr_reader :q, :page, :rows, :client, :thumb_klass
+    def initialize(q: 'record_type:secondary',
+                   page: 1,
+                   rows: 100,
                    client: SolrClient)
+      @q           = q
       @page        = page
+      @rows        = rows
       @client      = client
       @thumb_klass = thumb_klass
     end
 
     def stop?
-      response['numFound'] < 100
+      (response['numFound'] - (page * rows)) <= 0
     end
 
     def docs
@@ -25,7 +29,8 @@ module Umedia
       @response ||=
         (client.new.solr.get 'select',
           params: {
-            q: 'record_type:secondary',
+            q: q,
+            rows: rows,
             fl: 'id, object, child_viewer_types, kaltura_video'
           }
         )['response']
--- lib/tasks/thumbnails.rake
+++ lib/tasks/thumbnails.rake
@@ -1,16 +1,16 @@
 namespace :thumbnails do
   desc "cache all thumbnails"
-  task populate: [:environment]  do
-    thumbnails do |docs|
+  task :populate, [:query] => [:environment] do |task, args|
+    q = (args[:query]) ? args[:query] : '*:*'
+    puts "Thumbnail Query: #{q}"
+    thumbnails(q: q) do |docs|
       Umedia::CacheThumbnailsWorker.perform_async(docs)
     end
   end
 end
 
-
-
-def thumbnails(page: 0, &block)
-  search = Umedia::ThumbnailSearch.new(page: page)
-    yield(search.docs)
-    thumbnails(page: page + 1, &block) unless search.stop?
+def thumbnails(q: '*:*', page: 0, &block)
+  search = Umedia::ThumbnailSearch.new(q: q, page: page)
+  yield(search.docs)
+  thumbnails(q: q, page: page + 1, &block) unless search.stop?
 end
