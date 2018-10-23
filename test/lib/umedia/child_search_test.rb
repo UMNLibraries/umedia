9180dc6f1de525bf38808007337c6779332473c4
chadfennell <libsys@gmail.com>
2018-10-23 16:09:48 -0500

    child search now makes use of the rsolr pagination API

--- test/lib/umedia/child_search_test.rb
+++ test/lib/umedia/child_search_test.rb
@@ -13,7 +13,8 @@ module Umedia
       solr = Minitest::Mock.new
       client.expect :new, response, []
       response.expect :solr, solr, []
-      solr.expect :get, {'highlighting' => 'highlighting here', 'response' => { 'docs' =>[{id: 'sdfsdf:sdf'}]} }, ["child_search", {:params=>{:q=>"", :"q.alt"=>"*:*", :sort=>"child_index asc", :hl=>"on", :fl=>"title, id, object, parent_id, child_viewer_types, viewer_type", :"hl.method"=>"unified", :fq=>["parent_id:\"9942\""], :rows=>2000}}]
+      solr_args = [1, 3, "child_search", {:params=>{:q=>"", :"q.alt"=>"*:*", :sort=>"child_index asc", :hl=>"on", :fl=>"title, id, object, parent_id, first_viewer_type, viewer_type", :"hl.method"=>"unified", :fq=>["parent_id:\"9942\""]}}]
+      solr.expect :paginate, {'highlighting' => 'highlighting here', 'response' => { 'docs' =>[{id: 'sdfsdf:sdf'}]} }, solr_args
       search = ChildSearch.new(q: '', parent_id: parent_id, client: client, item_list_klass: item_list_klass)
       search.items.must_equal [{:foo=>"bar"}]
       search.highlighting.must_equal 'highlighting here'
