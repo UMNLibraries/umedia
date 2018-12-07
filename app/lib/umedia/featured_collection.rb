6b769a1af665b8a1080de4694afd9394ab750926
chadfennell <libsys@gmail.com>
2018-12-07 08:39:32 -0800

    still fleshing out the featured collection api and functionality; adds super collections concept

--- app/lib/umedia/featured_collection.rb
+++ app/lib/umedia/featured_collection.rb
@@ -3,32 +3,53 @@ module Umedia
   # total item count
   class FeaturedCollection
     attr_reader :collection,
+                :super_collections,
                 :search_klass,
-                :thumbnail_klass
+                :thumbnail_url_klass,
+                :item_klass
     def initialize(collection: :MISSING_COLLECTION,
                    search_klass: CollectionSampleSearch,
-                   thumbnail_klass: Thumbnail)
+                   thumbnail_url_klass: Thumbnail::Url,
+                   item_klass: Parhelion::Item,
+                   super_collections: SuperCollectionSearch.new.collections)
       @collection = collection
+      @super_collections = super_collections
       @search_klass = search_klass
-      @thumbnail_klass = thumbnail_klass
+      @thumbnail_url_klass = thumbnail_url_klass
+      @item_klass = item_klass
     end
 
-    def to_solr
+    def to_h
+      return {} unless item_count >= 3
       {
         id: "collection-#{collection.set_spec}",
         document_type: 'collection',
         set_spec: collection.set_spec,
         collection_name: collection.display_name,
         collection_description: collection.description,
-        collection_item_count: collection_item_count,
-        collection_thumbnails: thumbnail_config
+        collection_item_count: item_count,
+        contributing_organization_name: contributing_organization_name,
+        collection_thumbnails: thumbnail_config,
+        is_super_collection: super_collection?
       }
     end
 
     private
 
-    def collection_item_count
-      item_search.num_found
+    def super_collection?
+      super_collections.include? collection.display_name
+    end
+
+    def item_count
+      @item_count ||= item_search.num_found
+    end
+
+    def items
+      @items ||= item_search.items
+    end
+
+    def contributing_organization_name
+      items.first['contributing_organization_name']
     end
 
     def item_search
@@ -36,15 +57,14 @@ module Umedia
     end
 
     def thumbnail_config
-      item_search.items.map do |item|
-        thumb(item).to_h.merge(id: item['id'])
+      items.map do |item|
+        thumb(item).merge(id: item['id'])
       end.to_json
     end
 
     def thumb(item)
-      thumbnail_klass.new(object_url: item['object'],
-                          viewer_type: item['first_viewer_type'],
-                          entry_id: item['kaltura_video'])
+      thumbnail_url_klass.new(item: item_klass.new(doc_hash: item),
+                              iiif_thumb: true).to_h
     end
   end
 end
--- test/lib/umedia/featured_collection_test.rb
+++ test/lib/umedia/featured_collection_test.rb
@@ -2,41 +2,69 @@ require 'test_helper'
 
 module Umedia
   class CollectionIndexerTest < ActiveSupport::TestCase
-    it 'produces a featured collection solr config' do
-      collection = Collection.new(set_spec: 'foo:bar23', name: "foo col", description: "it's all cats")
 
+    let(:collection) { Collection.new(set_spec: 'foo:bar23', name: "foo col", description: "it's all cats") }
+    let(:doc_hash) { {'id' => 'foobrah:9er', 'object' => 'ugh', 'first_viewer_type' => 'image', 'kaltura_video' => '123123s'} }
+    let(:search_klass) { Minitest::Mock.new }
+    let(:search_klass_obj) { Minitest::Mock.new }
+    let(:item_klass) { Minitest::Mock.new }
+    let(:item_klass_obj) { Minitest::Mock.new }
+    let(:thumbnail_url_klass) { Minitest::Mock.new }
+    let(:thumbnail_url_klass_obj) { Minitest::Mock.new }
+
+   def setup
       # Sample Item Search mocks
-      search_klass = Minitest::Mock.new
-      search_klass_obj = Minitest::Mock.new
       search_klass.expect :new, search_klass_obj, [{set_spec: collection.set_spec}]
       search_klass_obj.expect :num_found, 103, []
-      items = [{'id' => 'foobrah:9er', 'object' => 'ugh', 'first_viewer_type' => 'image', 'kaltura_video' => '123123s'}]
+      items = [doc_hash]
       search_klass_obj.expect :items, items, []
 
+      # Item Mocks
+      item_klass.expect :new, item_klass_obj, [{ doc_hash: doc_hash }]
+
       # Thumbnail Mocks
-      thumbnail_klass = Minitest::Mock.new
-      thumbnail_klass_obj = Minitest::Mock.new
-      thumbnail_klass.expect :new, thumbnail_klass_obj, [{
-        object_url: 'ugh',
-        viewer_type: 'image',
-        entry_id: '123123s'
-      }]
-      thumbnail_klass_obj.expect :to_h, {stuff: 'here'}, []
+      thumbnail_url_klass.expect :new, thumbnail_url_klass_obj, [{ item: item_klass_obj, iiif_thumb: true }]
+      thumbnail_url_klass_obj.expect :to_h, {thumbnails: ['blah']}, []
+    end
 
+    describe 'when a collection is not a super collection' do
+      it 'produces a featured collection solr config and indicates it is not a super collection' do
+        featured = FeaturedCollection.new(
+          collection: collection,
+          search_klass: search_klass,
+          item_klass: item_klass,
+          thumbnail_url_klass: thumbnail_url_klass
+        )
 
-      featured = FeaturedCollection.new(
-        collection: collection,
-        search_klass: search_klass,
-        thumbnail_klass: thumbnail_klass
-      )
-
-      featured.to_solr.must_equal(
-        {:id=>"collection-foo:bar23", :document_type=>"collection", :set_spec=>"foo:bar23", :collection_name=>"foo col", :collection_description=>"it's all cats", :collection_item_count=>103, :collection_thumbnails=>"[{\"stuff\":\"here\",\"id\":\"foobrah:9er\"}]"}
-      )
+        featured.to_h.must_equal(
+          {:id=>"collection-foo:bar23", :document_type=>"collection", :set_spec=>"foo:bar23", :collection_name=>"foo col", :collection_description=>"it's all cats", :collection_item_count=>103, :contributing_organization_name=>nil, :collection_thumbnails=>"[{\"thumbnails\":[\"blah\"],\"id\":\"foobrah:9er\"}]", :is_super_collection=>false}
+        )
         search_klass.verify
-      search_klass_obj.verify
-      thumbnail_klass.verify
-      thumbnail_klass_obj.verify
+        search_klass_obj.verify
+        item_klass.verify
+        thumbnail_url_klass.verify
+        thumbnail_url_klass_obj.verify
+      end
+    end
+
+    describe 'when a collection is a super collection' do
+      it 'produces a featured collection solr config and indicates it is a super collection' do
+        featured = FeaturedCollection.new(
+          collection: collection,
+          search_klass: search_klass,
+          item_klass: item_klass,
+          thumbnail_url_klass: thumbnail_url_klass,
+          super_collections: ['foo col']
+        )
+
+        featured.to_h.must_equal(
+          {:id=>"collection-foo:bar23", :document_type=>"collection", :set_spec=>"foo:bar23", :collection_name=>"foo col", :collection_description=>"it's all cats", :collection_item_count=>103, :contributing_organization_name=>nil, :collection_thumbnails=>"[{\"thumbnails\":[\"blah\"],\"id\":\"foobrah:9er\"}]", :is_super_collection=>true}        )
+        search_klass.verify
+        search_klass_obj.verify
+        item_klass.verify
+        thumbnail_url_klass.verify
+        thumbnail_url_klass_obj.verify
+      end
     end
   end
 end
