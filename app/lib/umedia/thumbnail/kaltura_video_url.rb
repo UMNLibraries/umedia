942797287c90deaa80154239c74a39a12a46443e
chadfennell <libsys@gmail.com>
2019-01-09 11:08:02 -0600

    encapsulate logic for deriving an entry id from a given item within the kaltura vid url class

--- app/lib/umedia/thumbnail/kaltura_video_url.rb
+++ app/lib/umedia/thumbnail/kaltura_video_url.rb
@@ -3,13 +3,23 @@
 module Umedia
   module Thumbnail
     class KalturaVideoUrl
-      attr_reader :entry_id
-      def initialize(entry_id: :MISSING_ENTRY_ID)
-        @entry_id = entry_id
+      extend Forwardable
+      def_delegators :@item, :field_viewer_type
+      attr_reader :item
+      def initialize(item: :MISSING_ITEM)
+        @item = item
       end
 
       def to_s
         "https://cdnapisec.kaltura.com/p/1369852/thumbnail/entry_id/#{entry_id}"
+      end
+
+      def viewer_type
+        item.field_viewer_type.value.to_s
+      end
+
+      def entry_id
+        item.send(:"field_#{viewer_type}").value
       end
     end
   end
--- test/lib/umedia/thumbnail/kaltura_video_url_test.rb
+++ test/lib/umedia/thumbnail/kaltura_video_url_test.rb
@@ -4,8 +4,16 @@ module Umedia
   module Thumbnail
     class KalturaVideoUrlTest < ActiveSupport::TestCase
       it 'generates an kaltura video thumb url' do
-        url = KalturaVideoUrl.new(entry_id: '123459')
-        url.to_s.must_equal 'https://cdnapisec.kaltura.com/p/1369852/thumbnail/entry_id/123459'
+        field = Minitest::Mock.new
+        field.expect :value, 'fake_type_here', []
+        item = Minitest::Mock.new
+        item.expect :field_viewer_type, field, []
+        type_field = Minitest::Mock.new
+        type_field.expect :value, 'fake_entry_id_123', []
+        item.expect :field_fake_type_here, type_field, []
+
+        url = KalturaVideoUrl.new(item: item)
+        url.to_s.must_equal 'https://cdnapisec.kaltura.com/p/1369852/thumbnail/entry_id/fake_entry_id_123'
       end
     end
   end
