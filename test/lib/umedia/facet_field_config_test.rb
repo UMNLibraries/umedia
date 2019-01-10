832a74698f6ee979699f2dfe5239ce9102b5cc48
chadfennell <libsys@gmail.com>
2019-01-10 15:46:50 -0600

    list the featured collections first to help users see this facet is a special case

--- test/lib/umedia/facet_field_config_test.rb
+++ test/lib/umedia/facet_field_config_test.rb
@@ -4,7 +4,7 @@ module Umedia
   class FacetFieldConfigTest < ActiveSupport::TestCase
     it 'produces a set of common facet fields to be used throughout the app' do
       config = FacetFieldConfig.new
-      config.visible.must_equal([:types, :format_name, :date_created_ss, :subject_ss, :creator_ss, :publisher_s, :contributor_ss, :collection_name_s, :super_collection_name_ss, :page_count, :language])
+      config.visible.must_equal([:super_collection_name_ss, :types, :format_name, :date_created_ss, :subject_ss, :creator_ss, :publisher_s, :contributor_ss, :collection_name_s, :page_count, :language])
       config.hidden.must_equal([:format_name, :subject_fast_ss, :city, :state, :country, :region, :continent, :parent_collection_name, :contributing_organization_name])
       config.all.must_equal([:types, :format_name, :date_created_ss, :subject_ss, :creator_ss, :publisher_s, :contributor_ss, :collection_name_s, :super_collection_name_ss, :page_count, :language, :format_name, :subject_fast_ss, :city, :state, :country, :region, :continent, :parent_collection_name, :contributing_organization_name])
     end
