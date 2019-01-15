9f6114ef638a797bfd88cf7104326d74243d0236
chadfennell <libsys@gmail.com>
2019-01-15 11:56:43 -0600

    light refactoring; adds an all pages digitized display

--- app/helpers/about_helper.rb
+++ app/helpers/about_helper.rb
@@ -1,13 +1,25 @@
 module AboutHelper
   def contributing_organization_count
-    Rails.cache.fetch("contributing_organization_count", expires_in: 12.hours) do
-      Umedia::FacetCountSearch.new(facet: 'contributing_organization_name').count
-    end
+    facet_count('contributing_organization_name')
   end
 
   def formats_count
-    Rails.cache.fetch("formats_count", expires_in: 12.hours) do
-      Umedia::FacetCountSearch.new(facet: 'format_name').count
+    facet_count('format_name')
+  end
+
+  def all_pages_count
+    Rails.cache.fetch("all_pages_count", expires_in: 12.hours) do
+      count_delimit(Umedia::RecordCountSearch.new(include_children: true).count)
+    end
+  end
+
+  def count_delimit(count)
+    number_with_delimiter(count, delimiter: ',')
+  end
+
+  def facet_count(facet)
+    Rails.cache.fetch("facet_count_#{facet}", expires_in: 12.hours) do
+      count_delimit(Umedia::FacetCountSearch.new(facet: facet).count)
     end
   end
 end
