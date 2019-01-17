192f31388f5d9ac70bdfea7e5ec171e3f7fcf229
chadfennell <libsys@gmail.com>
2019-01-17 11:11:40 -0600

    bundle on docker load

---
+++ docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+# Exit on fail
+
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
+
+bundle check || bundle install --binstubs="$BUNDLE_BIN"
+# Ensure all gems installed. Add binstubs to bin which has been added to PATH in Dockerfile.
+
+exec "$@"
+# Finally call command issued to the docker service
