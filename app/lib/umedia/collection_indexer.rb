3e353ac119779945140cd50c1d19391dcb9a1bd5
chadfennell <libsys@gmail.com>
2019-01-29 11:43:01 -0600

    light refactoring

--- app/lib/umedia/collection_indexer.rb
+++ app/lib/umedia/collection_indexer.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 module Umedia
-  # Convert a collection into a featured collection and index it
   # And...upload a few example thumbnails for each featured collection
   class CollectionIndexer
-    attr_reader :collection,
-                :featured_collection_klass,
+    attr_reader :featured,
                 :solr,
                 :thumbnailer_klass
 
@@ -13,25 +11,20 @@ module Umedia
                    featured_collection_klass: FeaturedCollection,
                    solr: SolrClient.new,
                    thumbnailer_klass: Thumbnailer)
-      @collection = collection
-      @featured_collection_klass = featured_collection_klass
+      @featured = featured_collection_klass.new(collection: collection).to_h
       @solr = solr
       @thumbnailer_klass = thumbnailer_klass
     end
 
     def index!
-      add(featured_collection_klass.new(collection: collection).to_h)
+      return if featured.blank?
+
+      solr.add(featured)
+      upload_thumbnails(JSON.parse(featured[:collection_thumbnails]))
       solr.commit
     end
 
     private
-
-    def add(doc)
-      return if doc.blank?
-
-      solr.add(doc)
-      upload_thumbnails(JSON.parse(doc[:collection_thumbnails]))
-    end
 
     def upload_thumbnails(thumbs)
       thumbs.map do |thumb|
