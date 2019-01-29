feb766948cf97049e06bac0be65e3e98b2090dcb
chadfennell <libsys@gmail.com>
2019-01-29 11:15:20 -0600

    collections should know if they are a super collection and their total primary item count

--- app/lib/umedia/collection.rb
+++ app/lib/umedia/collection.rb
@@ -1,11 +1,33 @@
+# frozen_string_literal: true
+
 module Umedia
+  # Hang some UMedia-specific functionality on top of OAI Collections
   class Collection
-    attr_reader :set_spec, :name, :description
-    def initialize(set_spec: false, name: '', description: '')
+    attr_reader :set_spec,
+                :name,
+                :description,
+                :super_collection_names,
+                :collection_count_klass
+    def initialize(set_spec: false,
+                   name: '',
+                   description: '',
+                   super_collection_names: SuperCollections.new.names,
+                   collection_count_klass: CollectionCountSearch)
+
       raise ArgumentError.new("Required Argument: set_spec") unless set_spec
       @set_spec = set_spec
       @name = name
       @description = description
+      @super_collection_names = super_collection_names
+      @collection_count_klass = collection_count_klass
+    end
+
+    def item_count
+      collection_count_klass.new(set_spec: set_spec).to_i
+    end
+
+    def super_collection?
+      super_collection_names.include? display_name
     end
 
     # Since our CDM instance has both Reflections and UMedia content in it and
