fd7647b88d7d950071d91cf7259428c6aa0d703a
chadfennell <libsys@gmail.com>
2019-01-29 11:09:38 -0600

    add support for super collection sample items; general cleanup

--- app/lib/umedia/featured_collection.rb
+++ app/lib/umedia/featured_collection.rb
@@ -1,32 +1,46 @@
+# frozen_string_literal: true
+
 module Umedia
-  # A collection, plus a few example items with their thumb cofigs, and a
-  # total item count
+  # A kind of presenter for a UMedia UI element that displays collection
+  # metadata. The hash produced by to_h is what the UI template uses to
+  # display collection data
   class FeaturedCollection
+    extend Forwardable
+    def_delegators :@sample_items,
+                   :iiifables,
+                   :contributing_organization_name
+    def_delegators :@collection,
+                   :item_count,
+                   :set_spec,
+                   :description,
+                   :display_name,
+                   :super_collection?
+
     attr_reader :collection,
-                :super_collections,
-                :search_klass,
                 :thumbnail_url_klass,
-                :item_klass
+                :sample_klass,
+                :super_sample_klass
+
     def initialize(collection: :MISSING_COLLECTION,
-                   search_klass: CollectionSampleSearch,
-                   thumbnail_url_klass: Thumbnail::Url,
-                   item_klass: Parhelion::Item,
-                   super_collections: SuperCollectionSearch.new.collections)
+                   sample_klass: CollectionSampleItems,
+                   super_sample_klass: SuperCollectionSampleItems,
+                   thumbnail_url_klass: Thumbnail::Url)
       @collection = collection
-      @super_collections = super_collections
-      @search_klass = search_klass
       @thumbnail_url_klass = thumbnail_url_klass
-      @item_klass = item_klass
+      @sample_klass = sample_klass
+      @super_sample_klass = super_sample_klass
+      @sample_items = sample_items
     end
 
     def to_h
-      return {} unless item_count > 0
+      return {} if item_count <= 0
+
       {
-        id: "collection-#{collection.set_spec}",
+        id: "collection-#{set_spec}",
         document_type: 'collection',
-        set_spec: collection.set_spec,
-        collection_name: collection.display_name,
-        collection_description: collection.description,
+        set_spec: set_spec,
+        collection_name: display_name,
+        collection_description: description,
         collection_item_count: item_count,
         contributing_organization_name: contributing_organization_name,
         collection_thumbnails: thumbnail_config,
@@ -36,34 +50,22 @@ module Umedia
 
     private
 
-    def super_collection?
-      super_collections.include? collection.display_name
-    end
-
-    def item_count
-      @item_count ||= item_search.num_found
-    end
-
-    def items
-      @items ||= item_search.items
-    end
-
-    def contributing_organization_name
-      item_search.first_primary_item['contributing_organization_name']
-    end
-
-    def item_search
-      @item_search ||= search_klass.new(set_spec: collection.set_spec)
+    def sample_items
+      if super_collection?
+        super_sample_klass.new(collection_name: display_name)
+      else
+        sample_klass.new(set_spec: set_spec)
+      end
     end
 
     def thumbnail_config
-      items.map do |item|
+      iiifables.map do |item|
         thumb(item).merge(id: item['id'])
       end.to_json
     end
 
     def thumb(item)
-      thumbnail_url_klass.new(item: item_klass.new(doc_hash: item),
+      thumbnail_url_klass.new(item: item,
                               iiif_thumb: true).to_h
     end
   end
--- test/lib/umedia/featured_collection_test.rb
+++ test/lib/umedia/featured_collection_test.rb
@@ -1,91 +1,99 @@
 require 'test_helper'
 
 module Umedia
-  class CollectionIndexerTest < ActiveSupport::TestCase
+  class FeaturedCollectionTest < ActiveSupport::TestCase
+    describe 'when a standard collection is not empty' do
+      it 'produces a featured collection solr config' do
+        set_spec = "gergle:123"
+        sample_klass = Minitest::Mock.new
+        sample_klass_obj = Minitest::Mock.new
+        sample_klass.expect :new, sample_klass_obj, [{set_spec: set_spec}]
+        sample_klass_obj.expect :contributing_organization_name, 'Gopher Society', []
+        sample_klass_obj.expect :iiifables, ['one'], []
 
-    let(:collection) { Collection.new(set_spec: 'foo:bar23', name: "foo col", description: "it's all cats") }
-    let(:doc_hash) { {'id' => 'foobrah:9er', 'object' => 'ugh', 'first_viewer_type' => 'image', 'kaltura_video' => '123123s'} }
-    let(:search_klass) { Minitest::Mock.new }
-    let(:search_klass_obj) { Minitest::Mock.new }
-    let(:item_klass) { Minitest::Mock.new }
-    let(:item_klass_obj) { Minitest::Mock.new }
-    let(:thumbnail_url_klass) { Minitest::Mock.new }
-    let(:thumbnail_url_klass_obj) { Minitest::Mock.new }
+        collection = Minitest::Mock.new
+        collection.expect :item_count, 99, []
+        collection.expect :item_count, 99, []
+        collection.expect :set_spec, set_spec, []
+        collection.expect :set_spec, set_spec, []
+        collection.expect :set_spec, set_spec, []
+        collection.expect :description, "herper derp", []
+        collection.expect :display_name, 'pretty name here', []
+        collection.expect :super_collection?, false, []
+        collection.expect :super_collection?, false, []
 
-   def setup
-      # Sample Item Search mocks
-      search_klass.expect :new, search_klass_obj, [{set_spec: collection.set_spec}]
-      search_klass_obj.expect :num_found, 103, []
-      items = [doc_hash]
-      search_klass_obj.expect :items, items, []
+        # Thumbnail Mocks
+        thumbnail_url_klass = Minitest::Mock.new
+        thumbnail_url_klass_obj = Minitest::Mock.new
+        thumbnail_url_klass.expect :new, thumbnail_url_klass_obj, [ { item: 'one', iiif_thumb: true } ]
+        thumbnail_url_klass_obj.expect :to_h, { thumbnails: ['blah'] }, []
 
-      search_klass_obj.expect :first_primary_item, {'contributing_organization_name' => 'Fake Org Name Here'}, []
-
-      # Item Mocks
-      item_klass.expect :new, item_klass_obj, [{ doc_hash: doc_hash }]
-
-      # Thumbnail Mocks
-      thumbnail_url_klass.expect :new, thumbnail_url_klass_obj, [{ item: item_klass_obj, iiif_thumb: true }]
-      thumbnail_url_klass_obj.expect :to_h, {thumbnails: ['blah']}, []
-    end
-
-    describe 'when a collection is not a super collection' do
-      it 'produces a featured collection solr config and indicates it is not a super collection' do
         featured = FeaturedCollection.new(
           collection: collection,
-          search_klass: search_klass,
-          item_klass: item_klass,
+          sample_klass: sample_klass,
           thumbnail_url_klass: thumbnail_url_klass
         )
 
-        featured.to_h.must_equal(
-          {:id=>"collection-foo:bar23", :document_type=>"collection", :set_spec=>"foo:bar23", :collection_name=>"foo col", :collection_description=>"it's all cats", :collection_item_count=>103, :contributing_organization_name=>"Fake Org Name Here", :collection_thumbnails=>"[{\"thumbnails\":[\"blah\"],\"id\":\"foobrah:9er\"}]", :is_super_collection=>false}
-        )
-        search_klass.verify
-        search_klass_obj.verify
-        item_klass.verify
+        featured.to_h.must_equal({:id=>"collection-gergle:123", :document_type=>"collection", :set_spec=>"gergle:123", :collection_name=>"pretty name here", :collection_description=>"herper derp", :collection_item_count=>99, :contributing_organization_name=>"Gopher Society", :collection_thumbnails=>"[{\"thumbnails\":[\"blah\"],\"id\":null}]", :is_super_collection=>false})
+        sample_klass.verify
+        sample_klass_obj.verify
+        collection.verify
         thumbnail_url_klass.verify
         thumbnail_url_klass_obj.verify
+      end
+    end
+
+    describe 'when a standard collection is empty' do
+      it 'produces a featured collection solr config' do
+        collection = Minitest::Mock.new
+        collection.expect :item_count, 0, []
+        collection.expect :set_spec, "gergle:123", []
+        collection.expect :super_collection?, false, []
+        featured = FeaturedCollection.new(
+          collection: collection
+        )
+        featured.to_h.must_equal({})
+        collection.verify
       end
     end
 
     describe 'when a collection is a super collection' do
-      it 'produces a featured collection solr config and indicates it is a super collection' do
+      it 'produces a featured super collection solr config' do
+        set_spec = "gergle:123"
+        super_sample_klass = Minitest::Mock.new
+        super_sample_klass_obj = Minitest::Mock.new
+        super_sample_klass.expect :new, super_sample_klass_obj, [{:collection_name=>"display name here"}]
+        super_sample_klass_obj.expect :contributing_organization_name, 'Gopher Society', []
+        super_sample_klass_obj.expect :iiifables, ['one'], []
+
+        super_collection = Minitest::Mock.new
+        super_collection.expect :item_count, 99, []
+        super_collection.expect :item_count, 99, []
+        super_collection.expect :set_spec, set_spec, []
+        super_collection.expect :description, "herper derp", []
+        super_collection.expect :display_name, 'display name here', []
+        super_collection.expect :display_name, 'display name here', []
+        super_collection.expect :super_collection?, true, []
+        super_collection.expect :super_collection?, true, []
+
+        # Thumbnail Mocks
+        thumbnail_url_klass = Minitest::Mock.new
+        thumbnail_url_klass_obj = Minitest::Mock.new
+        thumbnail_url_klass.expect :new, thumbnail_url_klass_obj, [ { item: 'one', iiif_thumb: true } ]
+        thumbnail_url_klass_obj.expect :to_h, { thumbnails: ['blah'] }, []
+
         featured = FeaturedCollection.new(
-          collection: collection,
-          search_klass: search_klass,
-          item_klass: item_klass,
-          thumbnail_url_klass: thumbnail_url_klass,
-          super_collections: ['foo col']
+          collection: super_collection,
+          super_sample_klass: super_sample_klass,
+          thumbnail_url_klass: thumbnail_url_klass
         )
 
-        featured.to_h.must_equal(
-          {:id=>"collection-foo:bar23", :document_type=>"collection", :set_spec=>"foo:bar23", :collection_name=>"foo col", :collection_description=>"it's all cats", :collection_item_count=>103, :contributing_organization_name=>"Fake Org Name Here", :collection_thumbnails=>"[{\"thumbnails\":[\"blah\"],\"id\":\"foobrah:9er\"}]", :is_super_collection=>true}
-        )
-        search_klass.verify
-        search_klass_obj.verify
-        item_klass.verify
+        featured.to_h.must_equal({:id=>"collection-gergle:123", :document_type=>"collection", :set_spec=>"gergle:123", :collection_name=>"display name here", :collection_description=>"herper derp", :collection_item_count=>99, :contributing_organization_name=>"Gopher Society", :collection_thumbnails=>"[{\"thumbnails\":[\"blah\"],\"id\":null}]", :is_super_collection=>true})
+        super_sample_klass.verify
+        super_sample_klass_obj.verify
+        super_collection.verify
         thumbnail_url_klass.verify
         thumbnail_url_klass_obj.verify
-      end
-    end
-
-    describe 'when a collection has no items' do
-      it 'produces an empty hash' do
-        search_klass = Minitest::Mock.new
-        search_klass_obj = Minitest::Mock.new
-        search_klass.expect :new, search_klass_obj, [{set_spec: collection.set_spec}]
-        search_klass_obj.expect :num_found, 0, []
-        items = [doc_hash]
-        search_klass_obj.expect :items, items, []
-        featured = FeaturedCollection.new(
-          collection: collection,
-          search_klass: search_klass,
-          item_klass: item_klass,
-          thumbnail_url_klass: thumbnail_url_klass,
-          super_collections: ['foo col']
-        )
-        featured.to_h.must_equal({})
       end
     end
   end
