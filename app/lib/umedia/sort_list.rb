0c76822b937844318ec1f510bf2f634de74c2298
chadfennell <libsys@gmail.com>
2019-01-29 17:51:17 -0600

    sort on a sortable title string; we're going to have to create a title sort field that strips punctuation

--- app/lib/umedia/sort_list.rb
+++ app/lib/umedia/sort_list.rb
@@ -31,13 +31,13 @@ module Umedia
 
     def default_mappings
       [
-        {label: 'Relevance', sort: 'score desc, title desc' },
-        {label: 'Title: A to Z', sort: 'title asc, date_created_sort desc' },
-        {label: 'Title: Z to A', sort: 'title desc, date_created_sort desc' },
-        {label: 'Creator: A to Z', sort: 'creator_sort asc, title asc' },
-        {label: 'Creator: Z to A', sort: 'creator_sort desc, title asc' },
-        {label: 'Date: Oldest First', sort: 'date_created_sort asc, title asc' },
-        {label: 'Date: Newest First', sort: 'date_created_sort desc, title asc' }
+        {label: 'Relevance', sort: 'score desc, title_s desc' },
+        {label: 'Title: A to Z', sort: 'title_s asc, date_created_sort desc' },
+        {label: 'Title: Z to A', sort: 'title_s desc, date_created_sort desc' },
+        {label: 'Creator: A to Z', sort: 'creator_sort asc, title_s asc' },
+        {label: 'Creator: Z to A', sort: 'creator_sort desc, title_s asc' },
+        {label: 'Date: Oldest First', sort: 'date_created_sort asc, title_s asc' },
+        {label: 'Date: Newest First', sort: 'date_created_sort desc, title_s asc' }
       ]
     end
 
--- test/lib/umedia/sort_list_test.rb
+++ test/lib/umedia/sort_list_test.rb
@@ -4,13 +4,13 @@ module Umedia
     it 'creates a list of sort configurations' do
       query = Parhelion::Query.new
       sort_query_klass = Minitest::Mock.new
-      sort_query_klass.expect :new, nil, [{value:"score desc, title desc", query: query}]
-      sort_query_klass.expect :new, nil, [{value:"title asc, date_created_sort desc", query: query}]
-      sort_query_klass.expect :new, nil, [{value:"title desc, date_created_sort desc", query: query}]
-      sort_query_klass.expect :new, nil, [{value:"creator_sort asc, title asc", query: query}]
-      sort_query_klass.expect :new, nil, [{value:"creator_sort desc, title asc", query: query}]
-      sort_query_klass.expect :new, nil, [{value:"date_created_sort asc, title asc", query: query}]
-      sort_query_klass.expect :new, nil, [{value:"date_created_sort desc, title asc", query: query}]
+      sort_query_klass.expect :new, nil, [{value:"score desc, title_s desc", query: query}]
+      sort_query_klass.expect :new, nil, [{value:"title_s asc, date_created_sort desc", query: query}]
+      sort_query_klass.expect :new, nil, [{value:"title_s desc, date_created_sort desc", query: query}]
+      sort_query_klass.expect :new, nil, [{value:"creator_sort asc, title_s asc", query: query}]
+      sort_query_klass.expect :new, nil, [{value:"creator_sort desc, title_s asc", query: query}]
+      sort_query_klass.expect :new, nil, [{value:"date_created_sort asc, title_s asc", query: query}]
+      sort_query_klass.expect :new, nil, [{value:"date_created_sort desc, title_s asc", query: query}]
       labels = SortList.new(sort_query_klass: sort_query_klass,
                             query: query ).map { |sort| sort[:label] }
       labels.must_equal ["Relevance", "Title: A to Z", "Title: Z to A", "Creator: A to Z", "Creator: Z to A", "Date: Oldest First", "Date: Newest First"]
