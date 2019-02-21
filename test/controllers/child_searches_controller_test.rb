e7998c04af98581de4b7fbb2d08503a9e829315b
Chad Fennell <libsys@gmail.com>
2019-02-21 10:17:07 -0600

    now that we have an actual index, test against a real compound object

--- test/controllers/child_searches_controller_test.rb
+++ test/controllers/child_searches_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ChildSearchesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get child_searches_index_url, params: {id: 'foo'}
+    get child_searches_index_url, params: {id: 'p16022coll209:88'}
     assert_response :success
   end
 
