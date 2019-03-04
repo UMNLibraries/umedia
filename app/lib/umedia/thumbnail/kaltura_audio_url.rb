4dd63bec328a2f9cfd35ce214e8ea603503f7d0d
Chad Fennell <libsys@gmail.com>
2019-03-04 17:07:21 -0600

    thumbs need to be ssl friendly

--- app/lib/umedia/thumbnail/kaltura_audio_url.rb
+++ app/lib/umedia/thumbnail/kaltura_audio_url.rb
@@ -4,7 +4,7 @@ module Umedia
   module Thumbnail
     class KalturaAudioUrl
       def to_s
-        "http://d1rxd8nozvj6aj.cloudfront.net/umedia-audio-icon.png"
+        "https://d1rxd8nozvj6aj.cloudfront.net/umedia-audio-icon.png"
       end
     end
   end
