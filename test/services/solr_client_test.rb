5d39c3748cf02a684978e7bf53dd5e70e8a4a142
Chad Fennell <libsys@gmail.com>
2019-03-06 15:49:00 -0600

    reflects the new solr_test docker endpoint

--- test/services/solr_client_test.rb
+++ test/services/solr_client_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
   class SolrClientTest < ActiveSupport::TestCase
     it 'returns some the default UMedia solr config' do
       client = SolrClient.new
-      client.url.must_equal('http://solr:8983/solr/core')
+      client.url.must_equal('http://solr_test:8983/solr/core')
       client.solr.must_be_instance_of(RSolr::Client)
     end
   end
