5f6d0d540f75f0af02dcccd87251d758d51f23e2
Chad Fennell <libsys@gmail.com>
2019-03-06 15:35:56 -0600

    capybara power-up: support drag and drop

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -37,6 +37,21 @@ Capybara.register_driver :selenium do |app|
   )
 end
 
+module CapybaraExtension
+  def drag_by(right_by, down_by)
+    base.drag_by(right_by, down_by)
+  end
+end
+
+module CapybaraSeleniumExtension
+  def drag_by(right_by, down_by)
+    driver.browser.action.drag_and_drop_by(native, right_by, down_by).perform
+  end
+end
+::Capybara::Selenium::Node.send :include, CapybaraSeleniumExtension
+::Capybara::Node::Element.send :include, CapybaraExtension
+
+
 class ActiveSupport::TestCase
   # Make the Capybara DSL available in all integration tests
   include Capybara::DSL
