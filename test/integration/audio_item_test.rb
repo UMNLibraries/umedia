cf4da093c04c2094e962f9fb9edf72e47a387c32
Chad Fennell <libsys@gmail.com>
2019-03-06 15:47:04 -0600

    at least ensure the kaltura iframe is appearing

---
+++ test/integration/audio_item_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class AudioTest < ActiveSupport::TestCase
+  def teardown
+    super
+    Capybara.use_default_driver
+  end
+  it 'loads a kaltura audio player' do
+    Capybara.current_driver = :selenium
+    visit '/item/p16022coll171:1706'
+    sleep 1
+    has_selector?(:xpath, '//*[@id="kaltura_player_ifp"]').must_equal true
+  end
+end
