09538a09648fdef7766138f5ad89dc58f4d8dffe
Chad Fennell <libsys@gmail.com>
2019-10-19 16:00:17 -0700

    with compounds, you need to pick child pages

--- app/lib/umedia/super_collections.rb
+++ app/lib/umedia/super_collections.rb
@@ -21,7 +21,7 @@ module Umedia
         { name: "The Green Revolution", item_ids: ['p16022coll345:12027', 'p16022coll342:46679', 'p16022coll347:32763'] },
         { name: "African American Archival Materials", item_ids: ['p16022coll300:83', 'p16022coll226:107', 'p16022coll317:364'] },
         { name: "Seoul National University", item_ids: ['p16022coll175:20865', 'p16022coll175:20873', 'p16022coll175:20882'] },
-        { name: "Atlantic World", item_ids: ['p16022coll184:2441', 'p16022coll184:1854', 'p16022coll184:1796'] }
+        { name: "Atlantic World", item_ids: ['p16022coll460:496', 'p16022coll184:1854', 'p16022coll184:1348'] }
       ]
     end
   end
