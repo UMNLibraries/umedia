9f6991c2e17364e176d8bcd96d884455aab440be
Chad <fenne035@umn.edu>
2020-02-13 09:54:29 -0600

    test against the latest cloudfront distro

--- test/lib/umedia/thumbnail/url_test.rb
+++ test/lib/umedia/thumbnail/url_test.rb
@@ -14,7 +14,7 @@ module Umedia
             image_url_klass_obj.expect :to_s, 'imageurlhere', []
             url = Url.new(item: item, iiif_thumb: false, image_url_klass: image_url_klass)
             url.to_s.must_equal 'imageurlhere'
-            url.to_cdn_s.must_equal 'https://d2y23onbq5rnfc.cloudfront.net/87b9bff439a69511f7e7e545478972916af0b257.png'
+            url.to_cdn_s.must_equal 'https://dkp5i0hinw9br.cloudfront.net/87b9bff439a69511f7e7e545478972916af0b257.png'
             image_url_klass.verify
             image_url_klass_obj.verify
           end
@@ -29,7 +29,7 @@ module Umedia
             image_url_klass_obj.expect :to_s, 'imageurlhere', []
             url = Url.new(item: item, iiif_thumb: true, image_url_klass: image_url_klass)
             url.to_s.must_equal 'imageurlhere'
-            url.to_cdn_s.must_equal 'https://d2y23onbq5rnfc.cloudfront.net/87b9bff439a69511f7e7e545478972916af0b257.png'
+            url.to_cdn_s.must_equal 'https://dkp5i0hinw9br.cloudfront.net/87b9bff439a69511f7e7e545478972916af0b257.png'
             image_url_klass.verify
             image_url_klass_obj.verify
           end
@@ -46,7 +46,7 @@ module Umedia
           item = Parhelion::Item.new(doc_hash: {'id' => 'foo123:999', 'viewer_type' => 'kaltura_audio', 'page_count' => 0})
           url = Url.new(item: item, kaltura_audio_url_klass: kaltura_audio_url_klass)
           url.to_s.must_equal 'audiothumburlhere'
-          url.to_cdn_s.must_equal 'https://d2y23onbq5rnfc.cloudfront.net/f8f199197f33329f008e253414d520a70b5066ff.png'
+          url.to_cdn_s.must_equal 'https://dkp5i0hinw9br.cloudfront.net/f8f199197f33329f008e253414d520a70b5066ff.png'
           kaltura_audio_url_klass.verify
           kaltura_audio_url_klass_obj.verify
         end
@@ -62,7 +62,7 @@ module Umedia
           item = Parhelion::Item.new(doc_hash: {'id' => 'foo123:999', 'viewer_type' => 'kaltura_audio_playlist', 'page_count' => 0})
           url = Url.new(item: item, kaltura_audio_url_klass: kaltura_audio_url_klass)
           url.to_s.must_equal 'audiothumburlhere'
-          url.to_cdn_s.must_equal 'https://d2y23onbq5rnfc.cloudfront.net/f8f199197f33329f008e253414d520a70b5066ff.png'
+          url.to_cdn_s.must_equal 'https://dkp5i0hinw9br.cloudfront.net/f8f199197f33329f008e253414d520a70b5066ff.png'
           kaltura_audio_url_klass.verify
           kaltura_audio_url_klass_obj.verify
         end
@@ -78,7 +78,7 @@ module Umedia
           kaltura_video_url_klass_obj.expect :to_s, 'videourlhere', []
           url = Url.new(item: item, kaltura_video_url_klass: kaltura_video_url_klass)
           url.to_s.must_equal 'videourlhere'
-          url.to_cdn_s.must_equal 'https://d2y23onbq5rnfc.cloudfront.net/d055b959ed1bcb485d02e042b695f2fd131bc0b7.png'
+          url.to_cdn_s.must_equal 'https://dkp5i0hinw9br.cloudfront.net/d055b959ed1bcb485d02e042b695f2fd131bc0b7.png'
           kaltura_video_url_klass.verify
           kaltura_video_url_klass_obj.verify
         end
@@ -94,7 +94,7 @@ module Umedia
           kaltura_video_url_klass_obj.expect :to_s, 'videourlhere', []
           url = Url.new(item: item, kaltura_video_url_klass: kaltura_video_url_klass)
           url.to_s.must_equal 'videourlhere'
-          url.to_cdn_s.must_equal 'https://d2y23onbq5rnfc.cloudfront.net/d055b959ed1bcb485d02e042b695f2fd131bc0b7.png'
+          url.to_cdn_s.must_equal 'https://dkp5i0hinw9br.cloudfront.net/d055b959ed1bcb485d02e042b695f2fd131bc0b7.png'
           kaltura_video_url_klass.verify
           kaltura_video_url_klass_obj.verify
         end
@@ -110,7 +110,7 @@ module Umedia
           kaltura_video_url_klass_obj.expect :to_s, 'videourlhere', []
           url = Url.new(item: item, kaltura_video_url_klass: kaltura_video_url_klass)
           url.to_s.must_equal 'videourlhere'
-          url.to_cdn_s.must_equal 'https://d2y23onbq5rnfc.cloudfront.net/d055b959ed1bcb485d02e042b695f2fd131bc0b7.png'
+          url.to_cdn_s.must_equal 'https://dkp5i0hinw9br.cloudfront.net/d055b959ed1bcb485d02e042b695f2fd131bc0b7.png'
           kaltura_video_url_klass.verify
           kaltura_video_url_klass_obj.verify
         end
@@ -126,7 +126,7 @@ module Umedia
           item = Parhelion::Item.new(doc_hash: {'id' => 'foo123:999', 'viewer_type' => 'pdf', 'page_count' => 0})
           url = Url.new(item: item, pdf_url_klass: pdf_url_klass)
           url.to_s.must_equal 'pdfthumburlhere'
-          url.to_cdn_s.must_equal 'https://d2y23onbq5rnfc.cloudfront.net/5580c0ddfaa02a0c3581a5e9888e39eff4a259c9.png'
+          url.to_cdn_s.must_equal 'https://dkp5i0hinw9br.cloudfront.net/5580c0ddfaa02a0c3581a5e9888e39eff4a259c9.png'
           pdf_url_klass.verify
           pdf_url_klass_obj.verify
         end
@@ -142,7 +142,7 @@ module Umedia
           image_url_klass_obj.expect :to_s, 'imageurlhere', []
           url = Url.new(item: item, iiif_thumb: false, image_url_klass: image_url_klass)
           url.to_s.must_equal 'imageurlhere'
-          url.to_cdn_s.must_equal 'https://d2y23onbq5rnfc.cloudfront.net/87b9bff439a69511f7e7e545478972916af0b257.png'
+          url.to_cdn_s.must_equal 'https://dkp5i0hinw9br.cloudfront.net/87b9bff439a69511f7e7e545478972916af0b257.png'
           image_url_klass.verify
           image_url_klass_obj.verify
         end
