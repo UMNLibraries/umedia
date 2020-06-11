345cafe54fdbc497ed852040561ede0a298cfe73
Chad <fenne035@umn.edu>
2020-06-11 13:54:34 -0500

    don't push whenever cron tasks to staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,3 +5,14 @@ set :rails_env, "production"
 set :bundle_flags, '--deployment'
 set :keep_releases, 2
 set :branch, 'develop'
+
+
+# Don't set cron tasks for staging. Everything (indexing, solr backup, etc)
+# should be done manually in the staging environment.
+#
+# But why?
+#
+# * We don't want to stress the CONTENTdm server any more than we have to.
+# * We often want to see specific data tested / verified before pushing
+# to production, which hap
+Rake::Task["whenever:update_crontab"].clear_actions
