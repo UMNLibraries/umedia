f3481e9cc2128a94a2ff5c6a43f07ef9181e482b
David Naughton <nihiliad@gmail.com>
2021-05-14 10:37:47 -0500

    Clarifies and improves explanatory script text and input prompt.

--- local_dev_init.sh
+++ local_dev_init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo  "Runing this script with remove and replace your current solr dev and test instances."
-echo  "Run 'rake solr:backup' if you have a current working dev instance that you would like to retain."
-read -p "Are you sure? [y|n]" -n 1 -r
+echo  "This script will destroy and replace any already existing solr dev and test instances."
+echo  "Run 'rake solr:backup' first to retain an already existing dev instance."
+read -p "Continue, destroying any already existing instances? [y|n]" -n 1 -r
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
