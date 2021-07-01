e8854e75129424c719750e2c9ad3826e39902112
David Naughton <nihiliad@gmail.com>
2021-07-01 09:54:58 -0500

    Replaces some Minitest::Mock's with Mocha::Mock's.
    Made this change to eliminate screens full of warnings from the test output,
    all of the form `...forwarding to private method Minitest::Mock#{method_name}`.
    Mocha::Mock has much more robust handling of delegators declared with
    Forwardable.

--- test/lib/umedia/child_search_test.rb
+++ test/lib/umedia/child_search_test.rb
@@ -5,23 +5,23 @@ module Umedia
     it 'searches for children of a item' do
       parent_id = 9942
       q = ''
-      item_list_klass = Minitest::Mock.new
-      item_list_obj = Minitest::Mock.new
-      item_list_klass.expect :new, [{foo: 'bar'}], [{:results=>[{:id=>"sdfsdf:sdf"}]}]
-      client = Minitest::Mock.new
-      response = Minitest::Mock.new
-      solr = Minitest::Mock.new
-      client.expect :new, response, []
-      response.expect :solr, solr, []
+      item_list_klass = mock()
+      item_list_obj = mock()
+      item_list_klass.expects(:new).with({:results=>[{:id=>"sdfsdf:sdf"}]}).returns([{foo: 'bar'}]) 
+      client = mock()
+      response = mock()
+      solr = mock()
+      client.expects(:new).returns(response)
+      response.expects(:solr).returns(solr)
       search_params = {:q=>"", :sort=>"child_index asc", :hl=>"on", :fl=>"title, id, object, parent_id, first_viewer_type, viewer_type, child_index", :"hl.method"=>"unified", :fq=>["parent_id:\"9942\""]}
       solr_args = [1, 3, "child_search", {:params=>search_params}]
-      solr.expect :paginate, {'highlighting' => 'highlighting here', 'response' => { 'numFound' => 1, 'docs' =>[{id: 'sdfsdf:sdf'}]} }, solr_args
+      solr.expects(:paginate).with(solr_args).returns({'highlighting' => 'highlighting here', 'response' => { 'numFound' => 1, 'docs' =>[{id: 'sdfsdf:sdf'}]}})
 
-      search_config = Minitest::Mock.new
-      search_config.expect :to_h, search_params, []
-      search_config.expect :page, 1, []
-      search_config.expect :rows, 3, []
-      search_config.expect :fq, [], []
+      search_config = mock()
+      search_config.expects(:to_h).returns(search_params)
+      search_config.expects(:page).returns(1)
+      search_config.expects(:rows).returns(3)
+      search_config.expects(:fq)
 
       search = ChildSearch.new(parent_id: parent_id, search_config: search_config, client: client, item_list_klass: item_list_klass)
       _(search.items).must_equal [{:foo=>"bar"}]
@@ -29,11 +29,6 @@ module Umedia
       _(search.num_found).must_equal 1
 
       _(search.highlighting).must_equal 'highlighting here'
-      item_list_klass.verify
-      client.verify
-      response.verify
-      solr.verify
-      search_config.verify
     end
 
     describe 'when initialized without a parent id' do
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 require "minitest/autorun"
+require 'mocha/minitest'
 require 'webmock/minitest'
 
 WebMock.enable!
