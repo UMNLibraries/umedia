7e5f0d2c77af9905474a90722ce9717cde446855
David Naughton <nihiliad@gmail.com>
2021-07-01 14:07:40 -0500

    Skips a test that seems to rely on ephemeral data being stable.

--- test/services/search_with_children_test.rb
+++ test/services/search_with_children_test.rb
@@ -4,6 +4,7 @@ require 'test_helper'
 
 class SearchWithChilrenTest < ActiveSupport::TestCase
   it 'runs a search and returns child page data included in search results' do
+    skip 'seems to expect ids that do not change across test env builds, but it seems they do change'
     ids = SearchWithChildren.new(param_string: 'q=libraries', rows: 1)
                             .docs
                             .map { |item| item['id'] }
