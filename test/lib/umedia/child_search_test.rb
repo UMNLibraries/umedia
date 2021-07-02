fdbfaccba3383439a3a2f340cd3246316729d474
David Naughton <nihiliad@gmail.com>
2021-07-02 09:29:45 -0500

    Fixes Mocha::Mock-related failures in child_search_test.

--- test/lib/umedia/child_search_test.rb
+++ test/lib/umedia/child_search_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 module Umedia
   class ChildSearchTest < ActiveSupport::TestCase
-    it 'searches for children of a item' do
+    it 'searches for children of an item' do
       parent_id = 9942
       q = ''
       item_list_klass = mock()
@@ -14,14 +14,15 @@ module Umedia
       client.expects(:new).returns(response)
       response.expects(:solr).returns(solr)
       search_params = {:q=>"", :sort=>"child_index asc", :hl=>"on", :fl=>"title, id, object, parent_id, first_viewer_type, viewer_type, child_index", :"hl.method"=>"unified", :fq=>["parent_id:\"9942\""]}
-      solr_args = [1, 3, "child_search", {:params=>search_params}]
-      solr.expects(:paginate).with(solr_args).returns({'highlighting' => 'highlighting here', 'response' => { 'numFound' => 1, 'docs' =>[{id: 'sdfsdf:sdf'}]}})
+      solr.expects(:paginate)
+	.with(1, 3, "child_search", {:params=>search_params})
+	.returns({'highlighting' => 'highlighting here', 'response' => { 'numFound' => 1, 'docs' =>[{id: 'sdfsdf:sdf'}]}})
 
       search_config = mock()
       search_config.expects(:to_h).returns(search_params)
       search_config.expects(:page).returns(1)
       search_config.expects(:rows).returns(3)
-      search_config.expects(:fq)
+      search_config.expects(:fq).returns([])
 
       search = ChildSearch.new(parent_id: parent_id, search_config: search_config, client: client, item_list_klass: item_list_klass)
       _(search.items).must_equal [{:foo=>"bar"}]
