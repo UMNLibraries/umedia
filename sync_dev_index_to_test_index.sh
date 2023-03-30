544a673da9529e7a8a5421b545fdc3bb8585e2e4
Michael Berkowski <mjb@umn.edu>
2023-03-30 16:14:00 -0500

    Setup solr backup/restore in dev/test to use the same named snap all the time

--- sync_dev_index_to_test_index.sh
+++ sync_dev_index_to_test_index.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-docker-compose exec app bundle exec rake solr:backup; sudo chmod -R 777 snapshots; cp -r snapshots/* snapshots_test; ./docker-compose-test-exec app bundle exec rake solr:restore
+# Create a snapshot of the dev index at snapshots/snapshot.umedia_dev_snap
+# Copy it to snapshots_test
+# Restore it into the test index to keep in sync with dev
+#
+# THIS IS ONLY NEEDED WHEN YOU ADD NEW TEST RECORDS
+docker-compose exec app bundle exec rake solr:backup[umedia_dev_snap]; \
+  sudo chmod -R 777 snapshots/snapshot.umedia_dev_snap; \
+  cp -r snapshots/snapshot.umedia_dev_snap snapshots_test; \
+  ./docker-compose-test-exec app bundle exec rake solr:restore[umedia_dev_snap]
