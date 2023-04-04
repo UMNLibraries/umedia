4e557a33d5312d0a9bdd1817af239a122997a2db
Michael Berkowski <mjb@umn.edu>
2023-04-04 09:53:28 -0500

    attempt to sync named snapshot

--- sync_dev_index_to_test_index.sh
+++ sync_dev_index_to_test_index.sh
@@ -4,7 +4,7 @@
 # Restore it into the test index to keep in sync with dev
 #
 # THIS IS ONLY NEEDED WHEN YOU ADD NEW TEST RECORDS
-docker-compose exec app bundle exec rake solr:backup[umedia_dev_snap]; \
-  sudo chmod -R 777 snapshots/snapshot.umedia_dev_snap; \
-  cp -r snapshots/snapshot.umedia_dev_snap snapshots_test; \
+docker-compose exec app bundle exec rake solr:backup; \
+  sudo chmod -R 777 "snapshots/$(ls -1t snapshots|head -n1)"; \
+  git rm -r snapshots_test/umedia_dev_snap && cp -r snapshots/$(ls -1t snapshots|head -n1) snapshots_test/snapshot.umedia_dev_snap; \
   ./docker-compose-test-exec app bundle exec rake solr:restore[umedia_dev_snap]
