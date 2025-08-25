b91a58985d8b74116022c0cd065792dbc10eec78
Michael Berkowski <mjb@umn.edu>
2025-08-25 12:09:59 -0500

    No local capistrano, use umedia-deploy instead

--- config/deploy/production.rb
+++
@@ -1,6 +0,0 @@
-server "lib-umedia-prd-01.oit.umn.edu", user: "swadm", roles: %w{app db web}
-set :deploy_to, "/swadm/var/www/app"
-set :use_sudo, false
-set :rails_env, "production"
-set :bundle_flags, '--deployment'
-set :keep_releases, 3
